1b58e2e1e2bea70602ed94b5cadc06ea37fd0b22
ShrookZein <65206123+ShrookZein@users.noreply.github.com>
2024-05-03 02:41:15 +0300

    init

---
+++ src/main/java/com/global/Internet_Banking_System/InternetBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.global.Internet_Banking_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingSystemApplication.class, args);
+	}
+
+}
